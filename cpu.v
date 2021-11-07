ede496d4af06895bcdc914bf702475a01c7b3c12
Jules Maselbas <jmaselbas@zdiv.net>
2021-11-07 15:56:11 +0100

    Rework cpu stages

--- cpu.v
+++ cpu.v
@@ -2,33 +2,31 @@ module cpu(rst, clk);
 input rst, clk;
 
 reg   rden, wren;
-
-wire [31:0] out;
+reg [31:0] pc;
 
 /* fetch output */
-wire [31:0] data_o;
+reg [6:0]   fetch_addr;
+wire [31:0]  f_insn;
 
-/* decode output */
-wire [4:0]  opcode;
-wire [3:0]  alu_op;
-wire        invalid;
-wire [4:0]  d_rs1, d_rs2, d_rd;
-wire [31:0] d_reg1, d_reg2, d_imm;
-reg [31:0]  d_val1, d_val2;
+/* decode internal wire */
+wire [4:0]  opcode_w;
+wire [3:0]  alu_op_w;
+wire        invalid_w;
+wire [4:0]  rs1_w, rs2_w, rd_w;
+wire [31:0] reg1_w, reg2_w, imm_w;
+/* decode output values */
 reg [4:0]   d_opcode;
+reg [31:0]  d_op_val1, d_op_val2;
 reg [3:0]   d_alu_op;
-wire [31:0] alu_in1;
-wire [31:0] alu_in2;
-reg [31:0]  d_imm_reg;
-reg [31:0]  reg1_val;
+reg [4:0]   d_rd;
+reg [31:0]  d_imm;
 
 /* execute output */
 wire [31:0] alu_out;
-reg [6:0]   fetch_addr;
-wire [31:0] fetch_insn;
-reg [31:0]  pc;
+reg [4:0]   x_opcode;
+reg [4:0]   x_rd;
+reg [31:0]  x_imm;
 wire [31:0] rf_in;
-
 reg [31:0]  wb_val;
 
 parameter FETCH_INSN = 0;
@@ -36,20 +34,19 @@ parameter DECODE_AND_REGFILE_FETCH = 1;
 parameter EXECUTE = 2;
 parameter WRITE_BACK = 3;
 
-decode decode(data_o, opcode, alu_op, invalid, d_rd, d_rs1, d_rs2, d_imm);
-regfile regfile(rst, clk, wren, rden, d_rd, d_rs1, d_rs2, rf_in, d_reg1, d_reg2);
-alu alu(rst, clk, alu_op, alu_in1, alu_in2, alu_out);
-rom rom(clk, rst, fetch_addr, data_o);
-
-assign alu_in1 = d_reg1;
-assign alu_in2 = (opcode == 5'b00100) ? d_imm : d_reg2;
+decode decode(f_insn, opcode_w, alu_op_w, invalid, rd_w, rs1_w, rs2_w, imm_w);
+regfile regfile(rst, clk, wren, rden, x_rd, rs1_w, rs2_w, rf_in, reg1_w, reg2_w);
+alu alu(rst, clk, d_alu_op, d_op_val1, d_op_val2, alu_out);
+rom rom(clk, rst, fetch_addr, f_insn);
 
 /* we write back alu_out in RF in the general case
  Except when:
   * executing JAL or JALR => we write pc + 4
   * executing AUIPC       => we write pc + imm
  */
-assign rf_in = (d_opcode == 5'b11011 || d_opcode == 5'b11001) ? pc + 4 : (d_opcode == 5'b00101) ? pc + $signed(d_imm_reg) : alu_out;
+assign rf_in = (x_opcode == 5'b11011 || x_opcode == 5'b11001) ? pc + 4 :
+	       (x_opcode == 5'b00101) ? pc + $signed(x_imm) :
+	       alu_out;
 
 reg [2:0]   state;
 always @(posedge clk) begin
@@ -60,7 +57,16 @@ always @(posedge clk) begin
 		pc <= 0;
 		fetch_addr <= 0;
 		d_opcode <= 0;
+		d_op_val1 <= 0;
+		d_op_val2 <= 0;
 		d_alu_op <= 0;
+		d_rd <= 0;
+		d_imm <= 0;
+		d_opcode <= 0;
+		d_alu_op <= 0;
+		x_opcode <= 0;
+		x_rd <= 0;
+		x_imm <= 0;
 	end else begin
 		case (state)
 		FETCH_INSN: begin
@@ -69,27 +75,39 @@ always @(posedge clk) begin
 			$display("fetching pc = %x\n", pc);
 			state <= DECODE_AND_REGFILE_FETCH;
 		end
+		/* {f_insn} */
 		DECODE_AND_REGFILE_FETCH: begin
+			d_opcode <= opcode_w;
+			d_rd <= rd_w;
+			d_imm <= imm_w;
+			d_alu_op <= alu_op_w;
+			d_op_val1 <= reg1_w;
+			if (opcode_w == 5'b00100) begin
+				d_op_val2 <= imm_w;
+			end else begin
+				d_op_val2 <= reg2_w;
+			end
 			rden <= 0;
 			state <= EXECUTE;
 		end
+		/* {d_opcode, d_rd, d_alu_op, d_op_val1, d_op_val2} */
 		EXECUTE: begin
-			d_opcode <= opcode;
-			d_alu_op <= alu_op;
-			d_imm_reg <= d_imm;
-			reg1_val <= d_reg1;
+			x_opcode <= d_opcode;
+			x_rd <= d_rd;
+			x_imm <= d_imm;
 			wren <= d_rd != 0;
 			state <= WRITE_BACK;
 		end
+		/* {x_opcode, x_rd, alu_out} */
 		WRITE_BACK: begin
-			if (d_opcode == 5'b11011) begin // JAL
-				pc <= pc + d_imm_reg;
-				fetch_addr <= (pc + d_imm_reg) >> 2;
-				$display("JAL branching to pc = %x\n", pc + d_imm_reg);
-			end else if (d_opcode == 5'b11001) begin // JALR
-				pc <= reg1_val + d_imm_reg;
-				fetch_addr <= (reg1_val + d_imm_reg) >> 2;
-				$display("JALR branching to pc = %x\n", reg1_val + d_imm_reg);
+			if (x_opcode == 5'b11011) begin // JAL
+				pc <= pc + x_imm;
+				fetch_addr <= (pc + x_imm) >> 2;
+				$display("JAL branching to pc = %x\n", pc + x_imm);
+			end else if (x_opcode == 5'b11001) begin // JALR
+				pc <= alu_out + x_imm;
+				fetch_addr <= (alu_out + x_imm) >> 2;
+				$display("JALR branching to pc = %x\n", alu_out + x_imm);
 			end else begin
 				pc <= pc + 4;
 				fetch_addr <= (pc + 4) >> 2;
